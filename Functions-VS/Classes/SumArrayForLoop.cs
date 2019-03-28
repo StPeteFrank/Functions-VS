791b1fdd9aa02453242cb10032f2fc5da6a46282
diazfrankp@gmail.com <diazfrankp@gmail.com>
2019-03-28 10:01:14 -0400

    SumArray

--- Functions-VS/Classes/SumArrayForLoop.cs
+++ Functions-VS/Classes/SumArrayForLoop.cs
@@ -6,12 +6,22 @@ namespace Functions_VS
 {
     public class SumArrayForLoop
     {
-         public int SumArray(int[] arr)
+        /* public int SumArray(int[] arr)
         {
             int sum = 0;
             for (int i = 0; i < arr.Length; i++)
             {
                 sum = sum + arr[i];
+            }
+            return sum;
+        }*/
+
+        public int SumArray(int[] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum = sum + a[i];
             }
             return sum;
         }
