f2853187fb54dc3a071ff0bc6ddb75fe3b911a06
diazfrankp@gmail.com <diazfrankp@gmail.com>
2019-04-01 15:46:55 -0400

    SumArrayForLoop daily.

--- Functions-VS/Classes/SumOddArrayForLoop.cs
+++ Functions-VS/Classes/SumOddArrayForLoop.cs
@@ -11,14 +11,12 @@ namespace Functions_VS.Classes
             int sum = 0;
             for (int i = 0; i < a.Length; i++)
             {
-                if(a[i] % 2 == 1)
+                if (a[i] % 2 == 1)
                 {
                     sum += a[i];
                 }
             }
             return sum;
         }
-
-
     }
 }
