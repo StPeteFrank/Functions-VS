7484792bc9b0032d33a0778fe90d24e20e14d8ef
diazfrankp@gmail.com <diazfrankp@gmail.com>
2019-04-03 08:50:21 -0400

    SumArrayForLoop Daily

--- Functions-VS/Classes/SumArrayForLoop.cs
+++ Functions-VS/Classes/SumArrayForLoop.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Functions_VS
 {
-    public class SumArrayForLoop
+   /* public class SumArrayForLoop
     {
         public int SumArray(int[] a)
         {
@@ -17,13 +18,26 @@ namespace Functions_VS
         }
       
         
-    }
+    }*/
+
+        public class SumArrayForLoop
+        {
+            public int SumArray(int[] a)
+            {
+                int sum = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    sum += a[i];
+                }
+                return sum;
+            }
+        }
 
         // or //
 
-        /* static int SumArray(int[] arr)
-         {
-             return arr.Sum();
-         }*/
-    }
-
+        /*public int SumArray(int[] arr)
+        {
+            return arr.Sum();
+        }
+    }*/
+}
