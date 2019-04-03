fb62719d9c1e74cfa3b1a5f3f6f2b9d70654216e
diazfrankp@gmail.com <diazfrankp@gmail.com>
2019-04-03 08:58:41 -0400

    SmallestInArray Daily.

--- Functions-VS/Classes/SmallestInArray.cs
+++ Functions-VS/Classes/SmallestInArray.cs
@@ -4,6 +4,21 @@ using System.Text;
 
 namespace Functions_VS.Classes
 {
+    /*public class SmallestInArray
+    {
+        public int Smallest(int[] a)
+        {
+            int b = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (b > a[i])
+                {
+                    b = a[i];
+                }
+            }
+            return b;
+        }
+    }*/
     public class SmallestInArray
     {
         public int Smallest(int[] a)
