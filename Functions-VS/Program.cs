547e294961626a4a40f8e993534cee2cb095a72c
diazfrankp@gmail.com <diazfrankp@gmail.com>
2019-04-04 11:14:56 -0400

    SmallestInArray daily.

--- Functions-VS/Program.cs
+++ Functions-VS/Program.cs
@@ -8,10 +8,10 @@ namespace Functions_VS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(new LargestInArray().Largest(new int[] { 2, 4, 6, 8, 10 }));
-           //Console.WriteLine(new LargestInArray().Largest(new int[] { 2, 4, 6 }));  
+           Console.WriteLine(new LargestInArray().Largest(new int[] { 2, 4, 6, 8, 10 }));
 
-           Console.WriteLine(new SmallestInArray().Smallest(new int[] { 10, 4, 7, 9, 3, -8, 6 }));
+            // Console.WriteLine(new SmallestInArray().Smallest(new int[] { 10, 4, 7, 9, 3, -8, 6 }));
+            Console.WriteLine(new SmallestInArray().Smallest(new int[] { 20, 10, 6, -9, 14, 0, 12 }));
 
            Console.WriteLine(new SumUsingForEach().SumForEach(new int[] { 10, 20, 30, 40 }));
 
