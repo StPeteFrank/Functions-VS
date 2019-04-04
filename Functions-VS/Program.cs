6a3cad0a930559a8b89e596601bb5db833366936
diazfrankp@gmail.com <diazfrankp@gmail.com>
2019-04-04 11:21:00 -0400

    SumForEach daily.

--- Functions-VS/Program.cs
+++ Functions-VS/Program.cs
@@ -10,10 +10,10 @@ namespace Functions_VS
         {
            Console.WriteLine(new LargestInArray().Largest(new int[] { 2, 4, 6, 8, 10 }));
 
-            // Console.WriteLine(new SmallestInArray().Smallest(new int[] { 10, 4, 7, 9, 3, -8, 6 }));
             Console.WriteLine(new SmallestInArray().Smallest(new int[] { 20, 10, 6, -9, 14, 0, 12 }));
 
-           Console.WriteLine(new SumUsingForEach().SumForEach(new int[] { 10, 20, 30, 40 }));
+            // Console.WriteLine(new SumUsingForEach().SumForEach(new int[] { 10, 20, 30, 40 }));
+            Console.WriteLine(new SumUsingForEach().SumForEach(new int[] { 10, 20, 30, 40 }));
 
            Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 20, 40, 60, 80 }));
 
