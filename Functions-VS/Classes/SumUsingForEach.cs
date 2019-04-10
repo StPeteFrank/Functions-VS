9170b7c4f9f973188c9e2e9090af3faf379c7f24
diazfrankp@gmail.com <diazfrankp@gmail.com>
2019-04-10 10:50:46 -0400

    SumArrayForLoop warm up

--- Functions-VS/Classes/SumUsingForEach.cs
+++ Functions-VS/Classes/SumUsingForEach.cs
@@ -4,18 +4,6 @@ using System.Text;
 
 namespace Functions_VS.Classes
 {
-   /* public class SumUsingForEach
-    {
-        public int SumForEach(int[] a)
-        {
-            int sum = 0;
-            foreach(int b in a)
-            {
-                sum += b;
-            }
-            return sum;
-        }
-    }*/
     public class SumUsingForEach
     {
         public int SumForEach(int [] a)
