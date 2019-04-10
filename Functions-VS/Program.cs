632f172d9a5edef658a9fd2881b1964c8b9cc5ae
diazfrankp@gmail.com <diazfrankp@gmail.com>
2019-04-10 10:34:25 -0400

    LarestInArray warm up

--- Functions-VS/Program.cs
+++ Functions-VS/Program.cs
@@ -8,6 +8,7 @@ namespace Functions_VS
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine(new LargestInArray().Largest(new int[] { 2, 4, 6, 8, 10, 1 }));
             Console.WriteLine(new LargestInArray().Largest(new int[] { 2, 4, 6, 8, 10, 1 }));
 
             Console.WriteLine(new SmallestInArray().Smallest(new int[] { 20, 10, 6, -9, 14, 0, 12 }));
