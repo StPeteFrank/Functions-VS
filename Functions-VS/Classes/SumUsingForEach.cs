ee1e2c1aaf31eb01c778fc73e904430fb01cadf0
diazfrankp@gmail.com <diazfrankp@gmail.com>
2019-04-15 13:14:34 -0400

    SumUsingForEach warm up.

--- Functions-VS/Classes/SumUsingForEach.cs
+++ Functions-VS/Classes/SumUsingForEach.cs
@@ -4,6 +4,18 @@ using System.Text;
 
 namespace Functions_VS.Classes
 {
+   /* public class SumUsingForEach
+    {
+        public int SumForEach(int [] a)
+        {
+            int sum = 0;
+            foreach(int b in a)
+            {
+                sum += b;
+            }
+            return sum;
+        }
+    }*/
     public class SumUsingForEach
     {
         public int SumForEach(int [] a)
